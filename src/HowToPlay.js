9129ad6120958178739ecd538dcd0599fbd116b5
o.hossain <omarhossain1610@gmail.com>
2024-08-13 14:26:09 -0400

    first commit

--- src/HowToPlay.js
+++ src/HowToPlay.js
@@ -7,7 +7,7 @@ function HowToPlay({ onClose }) {
       <div className="how-to-play-popup">
         <button className="close-btn" onClick={onClose}>X</button> {/* Close button */}
         <h2>How to Play</h2> {/* Title of the pop-up */}
-        <p>Use the arrow keys to move the ball through the maze. Your goal is to reach the end of the maze while leaving a trail. If you backtrack, the trail will disappear.</p> {/* Instructions */}
+        <p>Use arrow keys to move the ball through the maze. Your goal is to reach the end of the maze while leaving a trail. If you backtrack, the trail will disappear.</p> {/* Instructions */}
       </div>
     </div>
   );
