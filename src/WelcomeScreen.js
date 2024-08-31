deb5806e865c9101b6cfa1260d0b201d7ea48687
o.hossain <omarhossain1610@gmail.com>
2024-08-31 13:10:46 -0400

    BACKGRONUDDDD

--- src/WelcomeScreen.js
+++ src/WelcomeScreen.js
@@ -1,14 +1,16 @@
-import react, {useState} from 'react'; 
-import './App.css'; 
+import React from 'react';
+import './App.css';
 
-function WelcomeScreen({onPlay, onHowToPlay}){
-    return(
-        <div className='welcome-screen'>
-            <h1>Welcome to Maze Master</h1>  
-            <button className="btn play-btn" onClick={onPlay}>Play</button>
-            <button className="btn how-to-play-btn" onClick={onHowToPlay}>How to Play</button>  
-        </div>
-    );
+function WelcomeScreen({ onPlay, onHowToPlay }) {
+  return (
+    <div className="welcome-screen">
+      <div className="welcome-content">
+        <h1>Welcome to Maze Master</h1>
+        <button className="btn play-btn" onClick={onPlay}>Play</button>
+        <button className="btn how-to-play-btn" onClick={onHowToPlay}>How to Play</button>
+      </div>
+    </div>
+  );
 }
 
-export default WelcomeScreen; 
+export default WelcomeScreen;
