4cf1c58cc87e35c6b7503842db4ed87fe1626182
o.hossain <omarhossain1610@gmail.com>
2024-09-03 16:28:30 -0400

    Cabbage Commit

--- src/WelcomeScreen.js
+++ src/WelcomeScreen.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 
-function WelcomeScreen({ onPlay, onHowToPlay }) {
+// Destructure the new prop `isHowToPlayVisible` to control visibility
+function WelcomeScreen({ onPlay, onHowToPlay, isHowToPlayVisible }) {
   return (
-    <div className="welcome-screen">
+    <div className={`welcome-screen ${isHowToPlayVisible ? 'hidden' : ''}`}>
       <div className="welcome-content">
-        <h1>Welcome to Maze Master</h1>
-        <button className="btn play-btn" onClick={onPlay}>Play</button>
-        <button className="btn how-to-play-btn" onClick={onHowToPlay}>How to Play</button>
+        {/* Conditionally hide/show the title and buttons */}
+        {!isHowToPlayVisible && (
+          <>
+            <h1>Welcome to Maze Master</h1>
+            <button className="btn play-btn" onClick={onPlay}>Play</button>
+            <button className="btn how-to-play-btn" onClick={onHowToPlay}>How to Play</button>
+          </>
+        )}
       </div>
     </div>
   );
